ee1c4368a3ca286936cefc58ed2043a4675b7967
k-roman5 <102798439+k-roman5@users.noreply.github.com>
2022-04-16 18:11:33 +0200

    Modification data.py, correction du code

--- data.py
+++ data.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import sqlite3
+import csv
 
 connexion = sqlite3.connect('table_repro.db')
 cursor = connexion.cursor()
 with open('Repro_IS.csv', 'r') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=';')
     for row in reader:
-    query = 'SELECT (id) INTO station WHERE'
-        nom = "{}"'.format(row['Station']) result=cursor.execute(query)
+        query = 'SELECT (id) INTO station WHERE nom = "{}"'.format(
+            row['Station'])
+        result = cursor.execute(query)
+
 if result.fetchone() == None:
