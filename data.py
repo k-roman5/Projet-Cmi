66573a623fdbb076557801ae36fc57208a11d76d
k-roman5 <102798439+k-roman5@users.noreply.github.com>
2022-04-16 18:56:17 +0200

    creation table arbre

--- data.py
+++ data.py
@@ -5,11 +5,13 @@ import sqlite3
 connexion = sqlite3.connect('table_repro.db')
 cursor = connexion.cursor()
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS users(
-     id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-     name TEXT,
-     age INTERGER
-)
+CREATE TABLE IF NOT EXISTS arbre (
+id INTEGER PRIMARY KEY AUTOINCREMENT, 
+code TEXT NOT NULL,
+VH REAL,
+H REAL,
+SH REAL
+);
 """)
 connexion.commit()
 cursor = connexion.cursor()
