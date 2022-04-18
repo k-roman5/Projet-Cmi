90733e1f8ff75c5f4a89010620e3ad5330ff2e76
k-roman5 <102798439+k-roman5@users.noreply.github.com>
2022-04-18 17:28:37 +0200

    .

--- dataBis.py
+++ dataBis.py
@@ -9,16 +9,13 @@ cursor = connexion.cursor()
 with open(r'Repro_IS.csv') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=';')
     for row in reader:
-        print("Nouvelle ligne :")
         query = 'SELECT * FROM t_Station WHERE nom = "{}"'.format(
             row['Station'])
         res = cursor.execute(query)
         if res.fetchone() == None:  # if the station doesn't exist
-            print("res.fetchone() :", res.fetchone())
             new_station = (row['Station'], row['Range'], row['Altitude'])
             cursor.execute(
                 'INSERT INTO t_Station VALUES (?,?,?)', new_station)
-            print("Station créée")
             connexion.commit()
 
 
