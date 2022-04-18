cbdc1dc100c4e7b7084c1dddbd1319a90f297164
Yolux-path <99172326+Yolux-path@users.noreply.github.com>
2022-04-18 07:58:11 +0200

    Update dataBis.py

--- dataBis.py
+++ dataBis.py
@@ -1,13 +1,18 @@
 import sqlite3
 import csv
+from time import clock_getres
 # CRUD : create, read, update, delete
 
 connexion = sqlite3.connect('table_repro_IS.db')
 cursor = connexion.cursor()
 
-my_code = ("213",)
-cursor.execute(' SELECT * FROM t_arbre WHERE code_arbre = ? ', my_code)
-
-print(cursor.fetchone())
+with open('Repro_IS.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=';')
+    for row in reader:
+        query = 'SELECT (id) INTO station WHERE nom = "{}"'.format(
+            row['Station'])
+        res = cursor.execute(query)
+        if res.fetchone() == None:
+            print(row['Station'])
 
 connexion.close()
