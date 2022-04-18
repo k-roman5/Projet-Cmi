bb51ea7d544e523051a49479c079be42ccf6b8b5
Yolux-path <99172326+Yolux-path@users.noreply.github.com>
2022-04-18 09:21:17 +0200

    J'ai essayé de gérer les cas où la station n'est pas présente dans notre table_Station

--- dataBis.py
+++ dataBis.py
@@ -6,13 +6,22 @@ import csv
 connexion = sqlite3.connect('table_repro_IS.db')
 cursor = connexion.cursor()
 
-with open('Repro_IS.csv', 'r') as csvfile:
+with open(r'C:\Users\Younes\Downloads\Repro_IS (2).csv') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=';')
     for row in reader:
-        query = 'SELECT (id) INTO station WHERE nom = "{}"'.format(
+        print("Nouvelle ligne :")
+        query = 'SELECT * FROM t_Station WHERE nom = "{}"'.format(
             row['Station'])
         res = cursor.execute(query)
-        if res.fetchone() == None:
-            print(row['Station'])
+        if res.fetchone() == None:  # if the station doesn't exist
+            print("res.fetchone() :", res.fetchone())
+            new_station = (cursor.lastrowid,
+                           row['Station'], row['Range'], row['Altitude'])
+            cursor.execute(
+                'INSERT INTO t_Station VALUES (?,?,?,?)', new_station)
+            print("Station créée")
+            connexion.commit()
+    connexion.commit()
+
 
 connexion.close()
