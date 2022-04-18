9e2504481b577613d845ff564239a3fef17d2370
Yolux-path <99172326+Yolux-path@users.noreply.github.com>
2022-04-18 07:21:03 +0200

    Update dataBis.py
    Toujours mon test

--- dataBis.py
+++ dataBis.py
@@ -2,7 +2,12 @@ import sqlite3
 import csv
 # CRUD : create, read, update, delete
 
-connexion = sqlite3.connect('table_repro_IS.db')
+connexion = sqlite3.connect('Vid1.db')
+cursor = connexion.cursor()
 
+my_code = ("213",)
+cursor.execute(' SELECT * FROM t_arbre WHERE code_arbre = ? ', my_code)
+
+print(cursor.fetchone())
 
 connexion.close()
