c2ed08a07962284ff17041f3f6afc8a98476f30b
Yolux-path <99172326+Yolux-path@users.noreply.github.com>
2022-04-18 18:08:52 +0200

    comms en +

--- dataBis.py
+++ dataBis.py
@@ -10,24 +10,26 @@ with open(r'Repro_IS.csv') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=';')
     for row in reader:
         req_station = 'SELECT * FROM t_Station WHERE nom = "{}"'.format(
-            row['Station'])
+            row['Station'])  # requete pour rechercher la station
         req_arbre = 'SELECT * FROM t_arbre WHERE code_arbre = "{}"'.format(
-            row['code'])
+            row['code'])  # requete pour rechercher l'arbre
 
-        res_station = cursor.execute(req_station)
-        res_arbre = cursor.execute(req_arbre)
+        res_station = cursor.execute(req_station)  # recherche de la station
+        res_arbre = cursor.execute(req_arbre)  # recherche de l'arbre
 
         if res_station.fetchone() == None:  # if the station doesn't exist
-            new_station = (row['Station'], row['Range'], row['Altitude'])
+            new_station = (row['Station'], row['Range'],
+                           row['Altitude'])  # create a new station
             cursor.execute(
-                'INSERT INTO t_Station VALUES (?,?,?)', new_station)
+                'INSERT INTO t_Station VALUES (?,?,?)', new_station)  # insert the new station
 
-        elif res_arbre.fetchone() == None:
-            new_arbre = (row['code'], row['VH'], row['H'], row['SH'])
+        elif res_arbre.fetchone() == None:  # if the arbre doesn't exist
+            new_arbre = (row['code'], row['VH'], row['H'],
+                         row['SH'])  # create a new arbre
             cursor.execute(
-                'INSERT INTO t_arbre VALUES (?,?,?,?)', new_arbre)
+                'INSERT INTO t_arbre VALUES (?,?,?,?)', new_arbre)  # insert the new arbre
 
-        connexion.commit()
+        connexion.commit()  # commit the changes
 
 
-connexion.close()
+connexion.close()  # close the connection
