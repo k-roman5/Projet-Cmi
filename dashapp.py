492b51d10a644bead1387b0f37ee1d50d114e8b3
k-roman5 <102798439+k-roman5@users.noreply.github.com>
2022-05-11 14:36:57 +0200

    Modification server

--- dashapp.py
+++ dashapp.py
@@ -1,5 +1,5 @@
-import model.data
-import view.GUI
+#import model.data
+#import view.GUI
 
 import dash
 from dash import html
@@ -53,49 +53,6 @@ app.layout = html.Div([
     sidebar,
     content,
 ])
-
-
-@app.callback(
-    Output("page-content", "children"),
-    [Input("url", "pathname")]
-)
-def render_page_content(pathname):
-    if pathname == "/":
-        dropdown = view.GUI.build_dropdown_menu(model.data.get_unique_values())
-        graph = view.GUI.init_graph()
-        return [
-            html.Div([
-
-                dropdown, graph
-            ])
-        ]
-
-    elif pathname == "/table":
-        # fetch client info
-        return [
-            html.H1('Données forêt pyrénnées (tableur)', id='table_view',
-                    style={'textAlign': 'left'}),
-            html.Hr(style={'width': '75%', 'align': 'center'}),
-            html.Div(id='data_table', children=view.GUI.data_table(model.data.df))
-        ]
-
-    else:
-        return html.Div(
-            [
-                html.H1("404: Not found", className="text-danger"),
-                html.Hr(),
-                html.P(f"The pathname {pathname} was not recognised..."),
-            ]
-        )
-
-
-@app.callback(
-    Output("bar-chart", "figure"),
-    [Input("dropdown", "value")])
-def update_bar_chart(value):
-    sub_df, attributes = model.data.extract_df(value)
-    return view.GUI.build_figure(sub_df, attributes)
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
