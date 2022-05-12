2709bcf3bdd87df9b2f90e52698122ca97dc4408
k-roman5 <102798439+k-roman5@users.noreply.github.com>
2022-05-12 13:35:21 +0200

    update data.py
    Création des tables

--- model/data.py
+++ model/data.py
@@ -1,10 +1,53 @@
 import sqlite3
 import csv
-
 # CRUD : create, read, update, delete
 
 connexion = sqlite3.connect('table_repro_IS.db')
 cursor = connexion.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS arbre (
+id INTEGER PRIMARY KEY AUTOINCREMENT, 
+code TEXT NOT NULL,
+VH REAL,
+H REAL,
+SH REAL
+);
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS station (
+id INTEGER PRIMARY KEY AUTOINCREMENT, 
+nom TEXT NOT NULL, 
+range INT NOT NULL,
+altitude INT NOT NULL
+);
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS vallee (
+id INTEGER PRIMARY KEY AUTOINCREMENT, 
+valley TEXT NOT NULL
+);
+''')
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS recolte (
+id INTEGER PRIMARY KEY AUTOINCREMENT, 
+harv_num INT NOT NULL,
+DD INT NOT NULL,
+harv INT NOT NULL,
+Year INT NOT NULL,
+Date DATE NOT NULL,
+Ntot1 INT NOT NULL,
+Ntot INT NOT NULL,
+Mtot REAL,
+oneacorn REAL,
+tot_Germ REAL,
+M_Germ REAL,
+N_Germ REAL,
+rate_Germ REAL
+);
+''')
 
 with open(r'Repro_IS.csv') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=';')
