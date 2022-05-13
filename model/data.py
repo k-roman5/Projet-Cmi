8bd4cc62c3bf15a1472151890469f1d56bd3b213
Yolux-path <99172326+Yolux-path@users.noreply.github.com>
2022-05-13 19:38:42 +0200

    Remplis de toutes les tables

--- model/data.py
+++ model/data.py
@@ -62,22 +62,21 @@ with open(r'Repro_IS.csv') as csvfile:
 
         if res_station.fetchone() == None:  # if the station doesn't exist
             print('La station {} n\'existe pas'.format(row['Station']))
-            new_station = (row['Station'], row['Range'],
+            new_station = (row['ID'], row['Station'], row['Range'],
                            row['Altitude'])  # create a new station
             print('station crée')
             cursor.execute(
-                'INSERT INTO t_Station VALUES (?,?,?)', new_station)  # insert the new station
+                'INSERT INTO t_Station VALUES (?,?,?,?)', new_station)  # insert the new station
             print("station insérée")
 
         if res_arbre.fetchone() == None:  # if the arbre doesn't exist
             print("l'arbre {} n'existe pas".format(row['code']))
-            new_arbre = (row['code'], row['VH'], row['H'],
+            new_arbre = (row['ID'], row['code'], row['VH'], row['H'],
                          row['SH'])  # create a new arbre
             if row['VH'] != None:  # if the arbre has a VH
                 cursor.execute(
-                    'INSERT INTO t_arbre VALUES (?,?,?,?)', new_arbre)  # insert the new arbre
+                    'INSERT INTO t_arbre VALUES (?,?,?,?,?)', new_arbre)  # insert the new arbre
                 print("arbre insérée")
     connexion.commit()  # commit the changes
-
 
 connexion.close()
--- model/dataBis.py
+++ model/dataBis.py
@@ -1,3 +1,5 @@
+from asyncio.windows_events import NULL
+from queue import Empty
 import sqlite3
 import csv
 
@@ -8,72 +10,61 @@ cursor = connexion.cursor()
 
 with open(r'Repro_IS.csv') as csvfile:
     reader = csv.DictReader(csvfile, delimiter=';')
-    for row in reader:  # new line in csv
-        print("new line")
+
+    def STATIONFUN(chosenrow1):
         req_station = 'SELECT * FROM t_Station WHERE nom = "{}"'.format(
-            row['Station'])  # requete pour rechercher la station
+            chosenrow1['Station'])
 
-        res_station = cursor.execute(req_station)  # recherche de la station
+        res_station = cursor.execute(req_station)
 
-        if res_station.fetchone() == None:  # if the station doesn't exist
-            print('La station {} n\'existe pas'.format(row['Station']))
-            new_station = (row['Station'], row['Range'],
-                           row['Altitude'])  # create a new station
-            print('station crée')
+        if res_station.fetchone() is None:
+            new_station = (chosenrow1['Station'],
+                           chosenrow1['Range'], chosenrow1['Altitude'])
             cursor.execute(
                 'INSERT INTO t_Station VALUES (?,?,?)', new_station)  # insert the new station
-            print("station insérée")
-        else:
-            print("station existante")
 
-        connexion.commit()  # commit the changes
-
-with open(r'Repro_IS.csv') as csvfile_aled:
-    reader_aled = csv.DictReader(csvfile_aled, delimiter=';')
-    for row in reader_aled:  # new line in csv
+    def TREEFUN(chosenrow2):
         req_arbre = 'SELECT * FROM t_arbre WHERE code_arbre = "{}"'.format(
-            row['code'])  # requete pour rechercher l'arbre
+            chosenrow2['code'])  # requete pour rechercher l'arbre
         res_arbre = cursor.execute(req_arbre)  # recherche de l'arbre
 
         if res_arbre.fetchone() == None:  # if the arbre doesn't exist
-            print("l'arbre {} n'existe pas".format(row['code']))
-            new_arbre = (row['code'], row['VH'], row['H'],
-                         row['SH'])  # create a new arbre
+            print("l'arbre {} n'existe pas".format(chosenrow2['code']))
+            new_arbre = (chosenrow2['code'], chosenrow2['VH'], chosenrow2['H'],
+                         chosenrow2['SH'])  # create a new arbre
             if row['VH'] != None:  # if the arbre has a VH
                 cursor.execute(
                     'INSERT INTO t_arbre VALUES (?,?,?,?)', new_arbre)  # insert the new arbre
                 print("arbre insérée")
-    connexion.commit()  # commit the changes
 
-with open(r'Repro_IS.csv') as csvfile_aled:
-    reader_aled = csv.DictReader(csvfile_aled, delimiter=';')
-    for row in reader_aled:  # new line in csv
+    def VELLEYFUN(chosenrow3):
         req_vallee = 'SELECT * FROM t_vallee WHERE valley = "{}"'.format(
-            row['Valley'])  # requete pour rechercher la vallee
+            chosenrow3['Valley'])  # requete pour rechercher la vallee
         res_vallee = cursor.execute(req_vallee)  # recherche de la vallee
 
         if res_vallee.fetchone() == None:  # if the valley doesn't exist
-            print("la vallee {} n'existe pas".format(row['Valley']))
-            new_vallee = (row['Valley'])  # create a new vallee
-            cursor.execute(
-                'INSERT INTO t_vallee VALUES (?)', new_vallee)  # insert the new valley
-            print("vallee insérée")
-    connexion.commit()  # commit the changes
+            new_vallee = (chosenrow3['Valley'])  # create a new vallee
+            cursor.execute('INSERT INTO t_vallee VALUES (?)',
+                           (new_vallee,))  # insert the new valley
 
-with open(r'Repro_IS.csv') as csvfile_aled:
-    reader_aled = csv.DictReader(csvfile_aled, delimiter=';')
-    for row in reader_aled:  # new line in csv
-        req_recolte = 'SELECT * FROM t_recolte WHERE numero = "{}"'.format(
-            row['harv_num'])  # requete pour rechercher la recolte
-        res_recolte = cursor.execute(req_recolte)  # recherche de la recolte
+    def RECOLTEFUN(chosenrow4):
+        new_recolte = (chosenrow4['harv_num'], chosenrow4['DD'], chosenrow4['harv'], chosenrow4['Year'], chosenrow4['Date'], chosenrow4['Ntot1'], chosenrow4['Ntot'],
+                       chosenrow4['Mtot'], chosenrow4['oneacorn'], chosenrow4['tot_Germ'], chosenrow4['M_Germ'], chosenrow4['N_Germ'], chosenrow4['rate_Germ'])  # create a new "recolte"
+        cursor.execute(
+            'INSERT INTO t_recolte VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)', new_recolte)  # insert the new "recolte"
+        print("recolte insérée")
 
-        if res_recolte.fetchone() == None:  # if the "recolte" doesn't exist
-            print("la recolte {} n'existe pas".format(row['code']))
-            new_recolte = (row['harv_num'], row['DD'], row['harv'], row['Year'], row['Date'], row['Ntot1'], row['Ntot'], row['Mtot'],
-                           row['oneacorn'], row['tot_Germ'], row['M_Germ'], row['N_Germ'], row['rate_Germ'])  # create a new "recolte"
-            cursor.execute(
-                'INSERT INTO t_recolte VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)', new_recolte)  # insert the new "recolte"
-            print("recolte insérée")
-    connexion.commit()  # commit the changes
+    def TREENONEFUN():
+        req_arbre_none = 'DELETE FROM t_arbre WHERE VH == NULL'
+        res_arbre_none = cursor.execute(req_arbre_none)
 
-connexion.close()  # close the connection
+    for row in reader:
+        STATIONFUN(row)
+        TREEFUN(row)
+       # TREENONEFUN()
+        VELLEYFUN(row)
+        RECOLTEFUN(row)
+
+        connexion.commit()
+
+    connexion.close()  # close the connection
